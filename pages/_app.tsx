b072595eb8e415edd42fa3589de8ba79a4a1b3e5
Zubair <zubairimtiaz395@gmail.com>
2023-01-11 06:48:48 +0500

    Added Tailwind + Husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Syne } from "@next/font/google";
+
+const syne = Syne({
+  subsets: ["latin"],
+  variable: "--font-syne",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${syne.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
